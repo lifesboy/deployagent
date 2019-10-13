f299c23aeaafe150da21d864a0288924d730a0bf
tuan.nv <tuan.nv@citigo.com.vn>
2019-10-13 11:11:39 +0700

    Add deployagent deploy api
    Add deployagent deploy api

---
+++ web/controllers/deployagentcontroller.py
@@ -0,0 +1,13 @@
+import os
+
+from web.app import app
+
+
+@app.route('/deployagent/deploy')
+def data_list():
+    os.system('cd /opt/deployagent/'
+              ' && mkdir /opt/deployagent/logs'
+              ' && git pull >> /opt/deployagent/logs/deployagent.txt'
+              ' && systemctl restart deployagent.service >> /opt/deployagent/logs/shipweb.txt'
+              ' && systemctl status deployagent.service >> /opt/deployagent/logs/shipweb.txt')
+    return '1'
