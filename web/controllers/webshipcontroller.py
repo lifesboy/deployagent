7513ca962c74b2705b6b5eeebf8ab8eb68a1fb77
tuan.nv <tuan.nv@citigo.com.vn>
2019-10-13 11:06:11 +0700

    update webship deploy script
    update webship deploy script

--- web/controllers/webshipcontroller.py
+++ web/controllers/webshipcontroller.py
@@ -6,7 +6,8 @@ from web.app import app
 @app.route('/webship/deploy')
 def data_list():
     os.system('cd /opt/shipweb-src/'
-              '&& git pull'
-              '&& rm -rf /opt/shipweb/*'
-              '&& cp -R release/* /opt/shipweb/')
+              ' && mkdir /opt/deployagent/logs'
+              ' && git pull >> /opt/deployagent/logs/shipweb.txt'
+              ' && rm -rfv /opt/shipweb/* >> /opt/deployagent/logs/shipweb.txt'
+              ' && cp -Rv /opt/shipweb-src/release/* /opt/shipweb/ >> /opt/deployagent/logs/shipweb.txt')
     return '1'
