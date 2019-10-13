ebef3cdec17a8d370643d3a2e11a755656b9c1f1
tuan.nv <tuan.nv@citigo.com.vn>
2019-10-13 15:17:13 +0700

    Enable Semaphore lock
    Enable Semaphore lock

--- web/controllers/deployagentcontroller.py
+++ web/controllers/deployagentcontroller.py
@@ -1,13 +1,17 @@
 import os
-
+import threading
 from web.app import app
+
+sem = threading.Semaphore()
 
 
 @app.route('/deployagent/deploy', methods=["GET", "POST"])
 def deployagent_deploy():
+    sem.acquire()
     os.system('cd /opt/deployagent/'
               # ' && mkdir /opt/deployagent/logs'
               ' && git pull >> /opt/deployagent/logs/deployagent.txt'
               ' && systemctl restart deployagent.service >> /opt/deployagent/logs/deployagent.txt'
               ' && systemctl status deployagent.service >> /opt/deployagent/logs/deployagent.txt')
+    sem.release()
     return '1'
--- web/controllers/shipwebcontroller.py
+++ web/controllers/shipwebcontroller.py
@@ -1,13 +1,16 @@
 import os
-
+import threading
 from web.app import app
 
+sem = threading.Semaphore()
 
 @app.route('/shipweb/deploy', methods=["GET", "POST"])
 def shipweb_deploy():
+    sem.acquire()
     os.system('cd /opt/shipweb-src/'
               # ' && mkdir /opt/deployagent/logs'
               ' && git pull >> /opt/deployagent/logs/shipweb.txt'
               ' && rm -rfv /opt/shipweb/* >> /opt/deployagent/logs/shipweb.txt'
               ' && cp -Rv /opt/shipweb-src/release/* /opt/shipweb/ >> /opt/deployagent/logs/shipweb.txt')
+    sem.release()
     return '1'
