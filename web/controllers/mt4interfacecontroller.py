596968464af7cffec987824b59f9bbc17ed49db7
tuan.nv <tuan.nv@citigo.com.vn>
2020-03-13 14:43:53 +0700

    temp comment git pull
    temp comment git pull

--- web/controllers/mt4interfacecontroller.py
+++ web/controllers/mt4interfacecontroller.py
@@ -8,8 +8,8 @@ sem = threading.Semaphore()
 def mt4interface_deploy():
     sem.acquire()
     os.system('cd /opt/mt4-interface/'
-              ' && mkdir /opt/deployagent/logs'
-              ' && git pull >> /opt/deployagent/logs/mt4-interface.txt'
+              #' && mkdir /opt/deployagent/logs'
+              #' && git pull >> /opt/deployagent/logs/mt4-interface.txt'
               ' && npm install >> /opt/deployagent/logs/mt4-interface.txt'
               ' && npm run build >> /opt/deployagent/logs/mt4-interface.txt'
               ' && mkdir /var/www/html/mt4-interface >> /opt/deployagent/logs/mt4-interface.txt'
