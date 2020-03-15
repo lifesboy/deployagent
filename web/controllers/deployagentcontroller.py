08fcf5e2cac396795c733357d53d3e16de188ee2
tuan.nv <tuan.nv@citigo.com.vn>
2020-03-15 22:25:23 +0700

    rollback update deployagent because restart itself doen not return log

--- web/controllers/deployagentcontroller.py
+++ web/controllers/deployagentcontroller.py
@@ -8,10 +8,10 @@ sem = threading.Semaphore()
 @app.route('/deployagent/deploy', methods=["GET", "POST"])
 def deployagent_deploy():
     sem.acquire()
-    build = os.popen('cd /opt/deployagent/'
+    os.system('cd /opt/deployagent/'
               # ' && mkdir /opt/deployagent/logs'
-              ' && git pull'
-              ' && systemctl restart deployagent.service'
-              ' && systemctl status deployagent.service').read()
+              ' && git pull >> /opt/deployagent/logs/deployagent.txt'
+              ' && systemctl restart deployagent.service >> /opt/deployagent/logs/deployagent.txt'
+              ' && systemctl status deployagent.service >> /opt/deployagent/logs/deployagent.txt')
     sem.release()
-    return "<br />".join(build.split("\n"))
+    return '1'
