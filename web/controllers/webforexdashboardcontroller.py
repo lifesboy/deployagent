ebf0097a587fd003b580bdf2c35ba6df04abf802
tuan.nv <tuan.nv@citigo.com.vn>
2020-03-15 23:40:14 +0700

    fix wrong dir

--- web/controllers/webforexdashboardcontroller.py
+++ web/controllers/webforexdashboardcontroller.py
@@ -14,6 +14,6 @@ def webforexdashboard_deploy():
               ' && npm run build_sharectv'
               #' && mkdir /var/www/html/web'
               ' && rm -rfv /var/www/html/dashboard/*'
-              ' && cp -Rv /opt/web/dist/dashboard/* /var/www/html/dashboard/').read()
+              ' && cp -Rv /opt/dashboard/dist/dashboard/* /var/www/html/dashboard/').read()
     sem.release()
     return "<br />".join(build.split("\n"))
